43c41a845aeb39bce6e038b033ef05eea6dc4452
ArturSkrzeta <65757653+ArturSkrzeta@users.noreply.github.com>
2020-09-09 13:18:42 +0200

    Add files via upload

---
+++ files/client.py
@@ -0,0 +1,39 @@
+import socket
+from uuid import uuid4
+from datetime import datetime
+
+HOST = '57.20.206.19'
+PORT = 33000
+BUFFER = 1024
+now = datetime.now()
+
+transactions = []
+
+def generate_transactions():
+    for _ in range(1,11):
+        t = uuid4()
+        transactions.append(str(t))
+
+def main():
+
+    generate_transactions()
+
+    # connecting to server
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST,PORT))
+
+    # client informs server about connection try
+    client_socket.send(str(now.strftime("%Y-%m-%d %H:%M:%S")).encode('utf8'))
+
+    # server inform client about successful connection
+    msg_from_server = client_socket.recv(BUFFER).decode('utf8')
+    print(msg_from_server)
+
+    # client sents data to server
+    transaction_str = ','.join(transactions)
+    client_socket.send(transaction_str.encode('utf8'))
+    print('Data sent to server')
+
+
+if __name__ == '__main__':
+    main()
---
+++ files/server.py
@@ -0,0 +1,64 @@
+import socket
+import sqlite3
+
+HOST = '57.20.206.19'
+PORT = 33000
+BUFFER = 1024
+msg = 'Welcome on server!'
+
+def establish_db():
+    global conn
+    conn = sqlite3.connect('data.db')
+    global c
+    c = conn.cursor()
+    try:
+        c.execute('CREATE TABLE tblTransactions(id INTEGER PRIMARY KEY AUTOINCREMENT, value TEXT)')
+    except:
+        pass
+
+def insert_into_db(data):
+    for row in data:
+        c.execute("INSERT INTO tblTransactions VALUES(NULL, '" + row + "')")
+    conn.commit()
+
+def terminate_db_connection():
+    c.close()
+    conn.close()
+
+def render_transactions(transactions_str):
+    transactions = []
+    transactions = transactions_str.split(',')
+    transaction_count = 1
+    for transaction in transactions:
+        print('Transaction ' + str(transaction_count) + ': ' + transaction + ' received.')
+        transaction_count += 1
+    return transactions
+
+def main():
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # allowing clients to connect the server socket
+    server_socket.bind((HOST,PORT))
+    # setting queue of at most 2 client sockets
+    server_socket.listen(2)
+
+    while True:
+
+        client_socket, address = server_socket.accept()
+
+        date = client_socket.recv(BUFFER).decode('utf8')
+        print(f'The connection from {address} has been established on {date}.')
+    
+        msg_to_client =  msg.encode('utf8')
+        client_socket.send(msg_to_client)
+
+        transactions_str = client_socket.recv(BUFFER).decode('utf8')
+        transactions = render_transactions(transactions_str)
+
+        # DB
+        establish_db()
+        insert_into_db(transactions)
+        terminate_db_connection()
+
+if __name__ == '__main__':
+    main()
